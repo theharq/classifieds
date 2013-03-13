c6d19c6360353c1519d53f1fd5cb71cc28c2d3a9
Hildebrando Rueda <harq.soft@gmail.com>
2013-03-12 22:30:30 -0500

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,25 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.12'
+gem 'pg'
+gem 'jquery-rails'
+
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+
+group :development, :test do
+  gem 'annotate'
+  gem 'factory_girl_rails'
+  gem 'rails_best_practices'
+  gem 'rspec-rails'
+end
+
+group :test do
+  gem 'simplecov'
+  gem 'shoulda-matchers'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Classifieds::Application.config.secret_token = '78bc1110d919cc88f866cc389551565987345cbf472f5129ca08bcb4b314a1a8563092c9fadbb1a96c19f98e305e90cc0d462e03a8a672c04430b1399699f4bd'

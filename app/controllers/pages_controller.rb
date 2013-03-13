5d11a938fd9f49737d762e86022a4a8e548c23d6
Hildebrando Rueda <hildebrando.rueda@koombea.com>
2013-03-13 18:13:59 -0500

    refactor crawler class

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
   def index
-    @classifieds = Crawler.get_page("http://clasificados.elheraldo.co/vivienda/77?sections=vivienda")
+    @classifieds = Crawler.get_ads_by_section("http://clasificados.elheraldo.co/vivienda/77?sections=vivienda")
   end
 end
--- lib/crawler.rb
+++ lib/crawler.rb
@@ -1,41 +1,42 @@
 class Crawler
 
-  def self.get_page(url)
-    @agent = Mechanize.new
-    @agent.user_agent = 'SW User-Agent'
-    @agent.open_timeout = 3
-    @agent.read_timeout = 4
-    @agent.keep_alive = false
-    @agent.max_history = 0
+  LAST_PAGE_LINK = "\u00FAltima \u00BB"
 
-    classifieds = []
+  def self.get_ads_by_section(url)
+    agent = Mechanize.new
+    crawler = Crawler.new
+    agent.user_agent = 'SW User-Agent'
+    agent.open_timeout = 3
+    agent.read_timeout = 4
+    agent.keep_alive = false
+    agent.max_history = 0
+    page = agent.get(url)
 
-    page = @agent.get(url)
-    last_page_link = page.link_with(:text => "\u00FAltima \u00BB")
+    classifieds = crawler.get_list_ads(page)
 
-    html_doc = Nokogiri::HTML(page.body, 'UTF-8')
+    last_page_number = crawler.get_last_page(page)
 
-    html_doc.search("ul.display_result li .datos").each do |item_list|
-     classifieds << Classified.new(content: item_list.css('p').text)
-    end
-
-    if last_page_link.present?
-      last_page_number = last_page_link.href.match(/page=(\d+)/)[1].to_i
-
+    if last_page_number.present?
       for pg_number in 1..last_page_number do
-
-        page = @agent.get("#{url}&page=#{pg_number}")
-
-        html_doc = Nokogiri::HTML(page.body, 'UTF-8')
-
-        html_doc.search("ul.display_result li .datos").each do |item_list|
-         classifieds << Classified.new(content: item_list.css('p').text)
-        end
-
+        page = agent.get("#{url}&page=#{pg_number}")
+        classifieds =  classifieds | crawler.get_list_ads(page)
       end
     end
 
     classifieds
+  end
+
+  def get_list_ads(page)
+    classifieds = []
+    Nokogiri::HTML(page.body, 'UTF-8').search("ul.display_result li .datos").each do |item_list|
+      classifieds << Classified.new(content: item_list.css('p').text)
+    end
+    classifieds
+  end
+
+  def get_last_page(page)
+    last_page_link = page.link_with(:text => LAST_PAGE_LINK )
+    return last_page_link.present? ? last_page_link.href.match(/page=(\d+)/)[1].to_i : nil
   end
 
 

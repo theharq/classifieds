adb5c60a14cc8b7d1dddf728194390facea543e6
Hildebrando Rueda <harq.soft@gmail.com>
2013-03-18 22:00:16 -0500

    include tag plugin for bootstrap

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,5 @@
 //= require jquery
 //= require jquery_ujs
 //= require twitter/bootstrap
+//= require bootstrap-tag
 //= require global
--- app/assets/javascripts/global.js
+++ app/assets/javascripts/global.js
@@ -1,11 +1,13 @@
 $(function(){ //ON DOCUMENT READY
 
   $(document).ajaxSend(function(event, request, settings) {
-   $('.loading').show();
-   });
+    $('.loading').show();
+  });
 
-   $(document).ajaxComplete(function(event, request, settings) {
-     $('.loading').hide();
-   });
+  $(document).ajaxComplete(function(event, request, settings) {
+    $('.loading').hide();
+  });
+
+  $('.input-tag').tag({placeholder: "Ingrese palabras clave"});
 
 }); // END ON READY
